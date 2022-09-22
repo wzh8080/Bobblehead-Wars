c73cd625c67efc05d435a7572750f5ba53bfb8e9
King <wzh8080@126.com>
2022-09-22 23:24:58 +0800

    4：Joints(End)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    private CharacterController characterController;
+    public Rigidbody head;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime; 
-        transform.position = pos;
+        // Vector3 pos = transform.position;
+        // pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        // pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime; 
+        // transform.position = pos;
+
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+
+        
+    }
+    void FixedUpdate() {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+        0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero) {
+            // TODO
+        } else {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
